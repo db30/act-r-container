4a23ce883f01bbf59461c135932c08bb613da846
db30 <45800724+db30@users.noreply.github.com>
2020-10-09 11:40:22 -0400

    Update start-it.sh

--- start-it.sh
+++ start-it.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -m
 
 if [ "$1" = '' ]
 
@@ -7,7 +8,7 @@ then
   sbcl --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (echo-act-r-output) (mp-print-versions) (run-node-env))'
 
 else 
-  sbcl --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (run-node-env))'  &
+  sbcl --non-interactive --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (run-node-env)(loop))'  &
   exec "$@"
 
 fi
