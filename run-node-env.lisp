92bf87251ad40153a0be87f96c254f2273592555
db30 <45800724+db30@users.noreply.github.com>
2020-10-09 16:35:58 -0400

    set the *allow-external-connections* flag t

--- run-node-env.lisp
+++ run-node-env.lisp
@@ -1,3 +1,5 @@
+(setf *allow-external-connections* t)
+
 (defun run-node-env ()
   (let ((home (sb-ext:posix-getenv "HOME")))
     (run-program "/bin/sh" (list (concatenate 'string home "/run-node.sh")) :wait nil) ;; :output *standard-output* for debugging
