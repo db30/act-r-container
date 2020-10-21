70c8ea70818d18a2edcadb86c3df9a1d56283a66
db30 <45800724+db30@users.noreply.github.com>
2020-10-21 15:24:31 -0400

    Update environment.js

--- environment.js
+++ environment.js
@@ -16,18 +16,9 @@
 
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http, {
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
-    }
-});
+var io = require('socket.io')(http);
 
+io.set('origins', '*:*');
 
 var expio = io.of('/expwindow');
 var envio = io.of('/environment');
