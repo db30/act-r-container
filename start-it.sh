780dd4f29082cbe6826e7dae128dae48903a9ca8
db30 <45800724+db30@users.noreply.github.com>
2020-10-22 17:20:00 -0400

    Don't need ACT-R output in mybinder
    Don't echo the output and send anything SBCL creates to /dev/null

--- start-it.sh
+++ start-it.sh
@@ -8,7 +8,7 @@ then
   sbcl --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (echo-act-r-output) (mp-print-versions) (run-node-env))'
 
 else 
-  sbcl --non-interactive --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (echo-act-r-output) (run-node-env) (loop))' > ~/debug.txt 2>&1 &
+  sbcl --non-interactive --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (run-node-env) (loop))' > /dev/null 2>&1 &
   exec "$@"
 
 fi
