bd9206d5567b428d62b7fbfe1d19815444ae45f7
db30 <db30@andrew.cmu.edu>
2020-10-23 15:16:21 -0400

    Move or remove the custom expwindow page as needed.

--- start-it.sh
+++ start-it.sh
@@ -6,10 +6,13 @@ if [ "$1" = '' ]
 
 then
   rm environment.html 
+  rm expwindow.html
   sbcl --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (echo-act-r-output) (mp-print-versions) (run-node-env))'
 
 else 
   mv environment.html actr7.x/examples/connections/nodejs/
+  mv expwindow.html actr7.x/examples/connections/nodejs/
+
   sbcl --non-interactive --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (run-node-env) (loop))' > /dev/null 2>&1 &
   exec "$@"
 
