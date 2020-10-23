d72bfeefb7981dd78444ee799085ed3d4a998e6e
db30 <45800724+db30@users.noreply.github.com>
2020-10-23 13:20:54 -0400

    Update start-it.sh
    Only move the custom environment.html if it's not the standard containered version and delete it otherwise.

--- start-it.sh
+++ start-it.sh
@@ -5,9 +5,11 @@ set -m
 if [ "$1" = '' ]
 
 then
+  rm environment.html 
   sbcl --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (echo-act-r-output) (mp-print-versions) (run-node-env))'
 
 else 
+  mv environment.html actr7.x/examples/connections/nodejs/
   sbcl --non-interactive --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (run-node-env) (loop))' > /dev/null 2>&1 &
   exec "$@"
 
