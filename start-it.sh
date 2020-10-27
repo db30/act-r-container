e8feaad03a91859f05aa693e2c65ee7949dbad91
db30 <db30@andrew.cmu.edu>
2020-10-27 10:48:59 -0400

    Move the appropriate environment interface to the connections dir.

--- start-it.sh
+++ start-it.sh
@@ -7,11 +7,14 @@ if [ "$1" = '' ]
 then
   rm environment.html 
   rm expwindow.html
+  mv environment-docked.html actr7.x/examples/connections/nodejs/
+
   sbcl --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (echo-act-r-output) (mp-print-versions) (run-node-env))'
 
 else 
   mv environment.html actr7.x/examples/connections/nodejs/
   mv expwindow.html actr7.x/examples/connections/nodejs/
+  rm environment-docked.html
 
   sbcl --non-interactive --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (run-node-env) (loop))' > /dev/null 2>&1 &
   exec "$@"
