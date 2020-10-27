e80f1f49a09de84a1b588c504cca86d44427f23a
db30 <db30@andrew.cmu.edu>
2020-10-27 11:57:37 -0400

    Put the Python files in a subdirectory, set the path, and preparing for creating a mount point in the non-mybinder version for tutorial files.

--- start-it.sh
+++ start-it.sh
@@ -7,15 +7,16 @@ if [ "$1" = '' ]
 then
   rm environment.html 
   rm expwindow.html
-  mv environment-docked.html actr7.x/examples/connections/nodejs/
-
+  cp -n -r -t actr7.x/tutorial actr7.x/original-tutorial/*
+  
   sbcl --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (echo-act-r-output) (mp-print-versions) (run-node-env))'
 
 else 
   mv environment.html actr7.x/examples/connections/nodejs/
   mv expwindow.html actr7.x/examples/connections/nodejs/
-  rm environment-docked.html
-
+  export PYTHONPATH=${PYTHONPATH}:${HOME}/actr-python-tutorial
+  mv actr7.x/original-tutorial actr7.x/tutorial
+  
   sbcl --non-interactive --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (run-node-env) (loop))' > /dev/null 2>&1 &
   exec "$@"
 
