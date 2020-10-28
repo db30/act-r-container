54b26ddbc4e0fefa5ce3ce34f6ce14c7a7061245
db30 <db30@andrew.cmu.edu>
2020-10-28 15:40:20 -0400

    Trying to set things up so you can start the Jupyter notebook server that's in the container.

--- start-it.sh
+++ start-it.sh
@@ -6,20 +6,43 @@ if [ "$1" = '' ]
 
 then
   rm environment.html 
-  rm expwindow.html
-  cp -n -r -t actr7.x/tutorial actr7.x/original-tutorial/*
+  rm environment-jupyter.html
   rm environment-play-with-docker.html
+  rm expwindow.html
+  rm expwindow-jupyter.html
+
+  cp -n -r -t actr7.x/tutorial actr7.x/original-tutorial/*
   
   sbcl --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (echo-act-r-output) (mp-print-versions) (run-node-env))'
 
 elif [ "$1" = "act-r.sh" ]
 then
   rm environment.html 
+  rm environment-jupyter.html
   rm expwindow.html
+  rm expwindow-jupyter.html
+
   mv actr7.x/original-tutorial actr7.x/tutorial
   mv environment-play-with-docker.html actr7.x/examples/connections/nodejs/environment.html
  
   /act-r.sh
+
+elif [ "$1" = "run-jupyter.sh" ]
+then
+
+
+  rm environment.html 
+  rm environment-play-with-docker.html
+  rm expwindow.html
+
+  mv environment-jupyter.html actr7.x/examples/connections/nodejs/environment.html
+  mv expwindow-jupyter.html actr7.x/examples/connections/nodejs/expwindow.html
+ 
+  cp -n -r -t actr7.x/tutorial actr7.x/original-tutorial/*
+
+  sbcl --non-interactive --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (run-node-env) (loop))' > /dev/null 2>&1 &
+
+  /run-jupyter.sh
 
 else 
   mv environment.html actr7.x/examples/connections/nodejs/
@@ -27,10 +50,11 @@ else
   export PYTHONPATH=${PYTHONPATH}:${HOME}/actr-python-tutorial
   mv actr7.x/original-tutorial actr7.x/tutorial
   rm environment-play-with-docker.html
+  rm expwindow-jupyter.html
+  rm environment-jupyter.html
   
   sbcl --non-interactive --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (run-node-env) (loop))' > /dev/null 2>&1 &
   
-  echo "$@" >> ~/actr7.x/jupyter-start.txt
   exec "$@"
 
 fi
