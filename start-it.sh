c27d4ae428f461aea8ef9994bcf37b1c9756854d
db30 <db30@andrew.cmu.edu>
2020-10-28 16:31:36 -0400

    Set the python path for the jupyter notebook branch.

--- start-it.sh
+++ start-it.sh
@@ -39,6 +39,7 @@ then
   mv expwindow-jupyter.html actr7.x/examples/connections/nodejs/expwindow.html
  
   cp -n -r -t actr7.x/tutorial actr7.x/original-tutorial/*
+  export PYTHONPATH=${PYTHONPATH}:${HOME}/actr-python-tutorial
 
   sbcl --non-interactive --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (run-node-env) (loop))' > /dev/null 2>&1 &
 
