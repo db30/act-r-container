385465be696296edec3b43c98e514bcd9eb41b0a
db30 <db30@andrew.cmu.edu>
2020-11-13 15:06:52 -0500

    Revert "Merge pull request #3 from db30/jupyter-test"
    This reverts commit 02df3aef16de1775ff04613871770bb0560ef67f, reversing
    changes made to d0f3dfc77adbc1c9f25e10d3e437dd44bc3696de.

--- run-jupyter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-set -m
-
-cd ${HOME}
- 
-jupyter notebook --ip=0.0.0.0 --port=8888 --NotebookApp.trust_xheaders=True --NotebookApp.allow_origin=*
--- start-it.sh
+++ start-it.sh
@@ -6,44 +6,20 @@ if [ "$1" = '' ]
 
 then
   rm environment.html 
-  rm environment-jupyter.html
-  rm environment-play-with-docker.html
   rm expwindow.html
-  rm expwindow-jupyter.html
-
   cp -n -r -t actr7.x/tutorial actr7.x/original-tutorial/*
+  rm environment-play-with-docker.html
   
   sbcl --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (echo-act-r-output) (mp-print-versions) (run-node-env))'
 
 elif [ "$1" = "act-r.sh" ]
 then
   rm environment.html 
-  rm environment-jupyter.html
   rm expwindow.html
-  rm expwindow-jupyter.html
-
   mv actr7.x/original-tutorial actr7.x/tutorial
   mv environment-play-with-docker.html actr7.x/examples/connections/nodejs/environment.html
  
   /act-r.sh
-
-elif [ "$1" = "run-jupyter.sh" ]
-then
-
-
-  rm environment.html 
-  rm environment-play-with-docker.html
-  rm expwindow.html
-
-  mv environment-jupyter.html actr7.x/examples/connections/nodejs/environment.html
-  mv expwindow-jupyter.html actr7.x/examples/connections/nodejs/expwindow.html
- 
-  cp -n -r -t actr7.x/tutorial actr7.x/original-tutorial/*
-  export PYTHONPATH=${PYTHONPATH}:${HOME}/actr-python-tutorial
-
-  sbcl --non-interactive --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (run-node-env) (loop))' > /dev/null 2>&1 &
-
-  /run-jupyter.sh
 
 else 
   mv environment.html actr7.x/examples/connections/nodejs/
@@ -51,11 +27,8 @@ else
   export PYTHONPATH=${PYTHONPATH}:${HOME}/actr-python-tutorial
   mv actr7.x/original-tutorial actr7.x/tutorial
   rm environment-play-with-docker.html
-  rm expwindow-jupyter.html
-  rm environment-jupyter.html
   
   sbcl --non-interactive --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (run-node-env) (loop))' > /dev/null 2>&1 &
-  
   exec "$@"
 
 fi
