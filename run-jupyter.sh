462df082f5ceb8286942d2c41d2d1628ebbd7ba1
db30 <db30@andrew.cmu.edu>
2020-12-09 13:49:11 -0500

    Trying to get it to open the initial page when under Docker.

--- run-jupyter.sh
+++ run-jupyter.sh
@@ -4,4 +4,4 @@ set -m
 
 cd ${HOME}
  
-jupyter notebook --ip=0.0.0.0 --port=8888 --NotebookApp.trust_xheaders=True --NotebookApp.allow_origin=*
+jupyter notebook --ip=0.0.0.0 --port=8888 --NotebookApp.trust_xheaders=True --NotebookApp.allow_origin=* simple.ipynb
