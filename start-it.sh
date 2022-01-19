df3bc0a3e6ecc2f530519200cc8ebdf34554ca20
db30 <45800724+db30@users.noreply.github.com>
2022-01-19 12:26:52 -0500

    Update start-it.sh
    use sed to edit the html env. files in place instead of having special ones

--- start-it.sh
+++ start-it.sh
@@ -5,38 +5,24 @@ set -m
 if [ "$1" = '' ]
 
 then
-  rm environment.html 
-  rm environment-jupyter.html
-  rm environment-play-with-docker.html
-  rm expwindow.html
-  rm expwindow-jupyter.html
-
+ 
   cp -n -r -t actr7.x/tutorial actr7.x/original-tutorial/*
   
   sbcl --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (echo-act-r-output) (mp-print-versions) (run-node-env))'
 
 elif [ "$1" = "act-r.sh" ]
 then
-  rm environment.html 
-  rm environment-jupyter.html
-  rm expwindow.html
-  rm expwindow-jupyter.html
-
+  
   mv actr7.x/original-tutorial actr7.x/tutorial
-  mv environment-play-with-docker.html actr7.x/examples/connections/nodejs/environment.html
+  sed -i -e "s/which_interface = 1/which_interface = 4/" -e "s/start-normal -->/start-normal/" -e "s/<\!-- end-normal/end-normal/" -e "s/<\!-- start-container/<\!-- start-container -->/" -e "s/end-container -->/<\!-- end-container -->/" actr7.x/examples/connections/nodejs/environment.html
  
   /act-r.sh
 
 elif [ "$1" = "run-jupyter.sh" ]
 then
 
-
-  rm environment.html 
-  rm environment-play-with-docker.html
-  rm expwindow.html
-
-  mv environment-jupyter.html actr7.x/examples/connections/nodejs/environment.html
-  mv expwindow-jupyter.html actr7.x/examples/connections/nodejs/expwindow.html
+  sed -i -e "s/which_interface = 1/which_interface = 3/" -e "s/start-normal -->/start-normal/" -e "s/<\!-- end-normal/end-normal/" -e "s/<\!-- start-container/<\!-- start-container -->/" -e "s/end-container -->/<\!-- end-container -->/" -e "s/src=\"\/socket.io/src=\"socket.io/" actr7.x/examples/connections/nodejs/environment.html
+  sed -i -e "s/which_interface = 1/which_interface = 3/" -e "s/src=\"\/socket.io/src=\"socket.io/" actr7.x/examples/connections/nodejs/expwindow.html
  
   cp -n -r -t actr7.x/tutorial actr7.x/original-tutorial/*
 
@@ -47,15 +33,13 @@ then
   /run-jupyter.sh
 
 else 
-  mv environment.html actr7.x/examples/connections/nodejs/
-  mv expwindow.html actr7.x/examples/connections/nodejs/
+
+  sed -i -e "s/which_interface = 1/which_interface = 2/" -e "s/start-normal -->/start-normal/" -e "s/<\!-- end-normal/end-normal/" -e "s/<\!-- start-container/<\!-- start-container -->/" -e "s/end-container -->/<\!-- end-container -->/" -e "s/src=\"\/socket.io/src=\"socket.io/" actr7.x/examples/connections/nodejs/environment.html
+  sed -i -e "s/which_interface = 1/which_interface = 2/" -e "s/src=\"\/socket.io/src=\"socket.io/" actr7.x/examples/connections/nodejs/expwindow.html
+  
   mv actr7.x/original-tutorial actr7.x/tutorial
 
   export PYTHONPATH=${PYTHONPATH}:${HOME}/actr7.x/tutorial/python
-
-  rm environment-play-with-docker.html
-  rm expwindow-jupyter.html
-  rm environment-jupyter.html
   
   sbcl --non-interactive --load "quicklisp/setup.lisp" --load "actr7.x/load-act-r.lisp" --eval '(progn (init-des) (run-node-env) (loop))' > /dev/null 2>&1 &
   
