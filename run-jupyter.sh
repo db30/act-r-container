545691e093cb485437baa28a89eaa59c29ed98e1
db30 <45800724+db30@users.noreply.github.com>
2024-10-03 10:56:13 -0400

    Update run-jupyter.sh for notebook 7
    Using the newer Python means that it will now get Jupyter notebook version 7 which requires changes to the passed parameters.

--- run-jupyter.sh
+++ run-jupyter.sh
@@ -4,4 +4,4 @@ set -m
 
 cd ${HOME}
  
-jupyter notebook  --NotebookApp.default_url="/notebooks/tutorial.ipynb" --ip=0.0.0.0 --port=8888 --NotebookApp.trust_xheaders=True --NotebookApp.allow_origin=*
+jupyter notebook  --ExtensionApp.default_url="/notebooks/tutorial.ipynb" --ip=0.0.0.0 --port=8888 --ServerApp.trust_xheaders=True --ServerApp.allow_origin=*
